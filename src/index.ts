0bad0981e9c5e46f43e8de2e9da0bf0ed28d29c0
Stammatime <stammer.3@osu.edu>
2020-11-06 19:29:58 -0500

    added authorization via JWT

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import jwt from 'express-jwt';
+
+declare global{
+    namespace Express {
+        export interface Request {
+           user?: Auths
+        }
+        export interface Auths {
+            canGet: boolean,
+            canPost: boolean,
+            canPut: boolean,
+            canDelete: boolean
+        }
+     }
+}
 
 const app = express();
 const port = 3000;
@@ -10,6 +25,8 @@ app.use(bodyParser.json());
 const myLogger = (req: any, res: any, next: any) => {
     // tslint:disable-next-line: no-console
     console.log(req.body);
+    // tslint:disable-next-line: no-console
+    console.log(req.user);
     next();
 }
 
@@ -22,29 +39,37 @@ items.set(1, "initialData");
 items.set(2, "testData");
 
 // Get all items
-app.get( "/widget", ( req, res ) => {
-    res.status(200).send(Object.fromEntries(items));
+app.get( "/widget", jwt({ secret: 'secretCode', algorithms: ['HS256'] }), ( req, res ) => {
+    if(req.user.canGet){
+        res.status(200).send(Object.fromEntries(items));
+    }
 } );
 
 // Add a new item
-app.post("/widget", ( req, res ) => {
-    items.set(id, req.body.item);
-    id++;
-    res.status(200).send(Object.fromEntries(items));
+app.post("/widget", jwt({ secret: 'secretCode', algorithms: ['HS256'] }), ( req, res ) => {
+    if(req.user.canPost){
+        items.set(id, req.body.item);
+        id++;
+        res.status(200).send(Object.fromEntries(items));
+    }
 } );
 
 // Update existing item
-app.put("/widget/:itemId", ( req, res ) => {
-    const itemId = parseInt(req.params.itemId, 10);
-    const item = items.get(itemId);
-    if(item) items.set(itemId, req.body.item);
-    res.status(200).send(Object.fromEntries(items));
+app.put("/widget/:itemId", jwt({ secret: 'secretCode', algorithms: ['HS256'] }), ( req, res ) => {
+    if(req.user.canPut){
+        const itemId = parseInt(req.params.itemId, 10);
+        const item = items.get(itemId);
+        if(item) items.set(itemId, req.body.item);
+        res.status(200).send(Object.fromEntries(items));
+    }
 } );
 
 // delete an item
-app.delete( "/widget/:itemId", ( req, res ) => {
-    items.delete(parseInt(req.params.itemId, 10));
-    res.status(200).send(Object.fromEntries(items));
+app.delete( "/widget/:itemId", jwt({ secret: 'secretCode', algorithms: ['HS256'] }), ( req, res ) => {
+    if(req.user.canDelete){
+        items.delete(parseInt(req.params.itemId, 10));
+        res.status(200).send(Object.fromEntries(items));
+    }
 } );
 
 // start the Express server
